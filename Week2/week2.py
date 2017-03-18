c8aa40b9dc160252fb2739cef2673e337d2f000f
Karl <karlzhou@gmail.com>
2017-03-19 01:18:55 +0800

    week 2 code cleanup

--- Week2/week2.py
+++ Week2/week2.py
@@ -1,18 +1,18 @@
-from week2_utility import *
+from Week2.week2_utility import *
 from utilities import ToSingleLineOfString
 
 
 def skew_problem():
     dna = 'GAGCCACCGCGATA'
     skew_values = skew(dna)
-    print ' '.join([str(value) for value in skew_values])
+    print(' '.join([str(value) for value in skew_values]))
 
 
 def minimum_skew_problem():
     with open('Datasets/MinimumSkewProblem_data02.txt', 'r') as datafile:
         dna = datafile.readline().strip()
     min_skew_indices = get_minimum_skews(dna)
-    print ToSingleLineOfString(min_skew_indices)
+    print(ToSingleLineOfString(min_skew_indices))
 
 
 def hamming_distance_problem():
@@ -20,7 +20,7 @@ def hamming_distance_problem():
         dna1 = datafile.readline().strip()
         dna2 = datafile.readline().strip()
     hamming = get_hamming_distance(dna1, dna2)
-    print hamming
+    print(hamming)
 
 
 def approximate_pattern_matching_problem():
@@ -29,14 +29,14 @@ def approximate_pattern_matching_problem():
         dna = datafile.readline().strip()
         d = int(datafile.readline().strip())
     matching_positions = approximate_pattern_matching(pattern, dna, d)
-    print ToSingleLineOfString(matching_positions)
+    print(ToSingleLineOfString(matching_positions))
 
 
 def count_approximate_pattern():
     pattern = 'AAAAA'
     dna = 'AACAAGCTGATAAACATTTAAAGAG'
     d = 2
-    print approximate_pattern_count(pattern, dna, d)
+    print(approximate_pattern_count(pattern, dna, d))
 
 
 def approximate_pattern_count_problem():
@@ -45,7 +45,7 @@ def approximate_pattern_count_problem():
         dna = datafile.readline().strip()
         d = int(datafile.readline().strip())
     count = approximate_pattern_count(pattern, dna, d)
-    print count
+    print(count)
 
 
 def frequent_words_with_mismatches_problem():
@@ -55,7 +55,7 @@ def frequent_words_with_mismatches_problem():
         k = int(params[0])
         d = int(params[1])
     words = frequent_words_with_mismatches(dna, k, d)
-    print ToSingleLineOfString(words)
+    print(ToSingleLineOfString(words))
 
 
 def frequent_words_with_mismatches_and_reverse_complements_problem():
@@ -65,7 +65,7 @@ def frequent_words_with_mismatches_and_reverse_complements_problem():
         k = int(params[0])
         d = int(params[1])
     words = frequent_words_with_mismatches_and_reverse_complements(dna, k, d)
-    print ToSingleLineOfString(words)
+    print(ToSingleLineOfString(words))
 
 frequent_words_with_mismatches_and_reverse_complements_problem()
 
@@ -74,34 +74,34 @@ def q2():
     dna1 = 'CTACAGCAATACGATCATATGCGGATCCGCAGTGGCCGGTAGACACACGT'
     dna2 = 'CTACCCCGCTGCTCAATGACCGGGACTAAAGAGGCGAAGATTATGGTGTG'
     hamming = get_hamming_distance(dna1, dna2)
-    print hamming
+    print(hamming)
 
 
 def q3():
-    print ToSingleLineOfString(get_minimum_skews('CATTCCAGTACTTCGATGATGGCGTGAAGA'))
+    print(ToSingleLineOfString(get_minimum_skews('CATTCCAGTACTTCGATGATGGCGTGAAGA')))
 
 
 def q4():
-    print str(approximate_pattern_count('CCC', 'CATGCCATTCGCATTGTCCCAGTGA', 2))
+    print(str(approximate_pattern_count('CCC', 'CATGCCATTCGCATTGTCCCAGTGA', 2)))
 
 
 def q5():
     neighbours = frequent_words_with_mismatches('TGCAT', 5, 2)
-    print len(neighbours)
+    print(len(neighbours))
 
 
 def quiz2_1():
-    print 'Q2: %d' % get_hamming_distance('CAGAAAGGAAGGTCCCCATACACCGACGCACCAGTTTA',
-                                          'CACGCCGTATGCATAAACGAGCCGCACGAACCAGAGAG')
+    print('Q2: %d' % get_hamming_distance('CAGAAAGGAAGGTCCCCATACACCGACGCACCAGTTTA',
+                                          'CACGCCGTATGCATAAACGAGCCGCACGAACCAGAGAG'))
 
 def quiz2_2():
-    print 'Q3: %s' % ToSingleLineOfString(get_minimum_skews('CATTCCAGTACTTCGATGATGGCGTGAAGA'))
+    print('Q3: %s' % ToSingleLineOfString(get_minimum_skews('CATTCCAGTACTTCGATGATGGCGTGAAGA')))
 
 def quiz2_3():
-    print 'Q4: %s' % str(approximate_pattern_count('CCC', 'CATGCCATTCGCATTGTCCCAGTGA', 2))
+    print('Q4: %s' % str(approximate_pattern_count('CCC', 'CATGCCATTCGCATTGTCCCAGTGA', 2)))
 
 def quiz2_4():
-    print 'Q5: %s' % len(get_neighbours('CCAGTCAATG', 1))
+    print('Q5: %s' % len(get_neighbours('CCAGTCAATG', 1)))
 
 def quiz2():
     quiz2_1()
