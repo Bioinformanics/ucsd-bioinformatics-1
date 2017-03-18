ce111612651084b3bb1bc8a8865c1883d8918e71
Karl <karlzhou@gmail.com>
2017-03-18 11:41:28 +0800

    update E-coli clamp count

--- Week1/week1_honor.py
+++ Week1/week1_honor.py
@@ -3,6 +3,6 @@ from week1_utility import *
 def find_clump_ecoli():
     with open('E_coli.txt', 'r') as datafile:
         dna = datafile.readline()
-    find_clump_efficient(dna, 9, 3, 500)
+    find_clump(dna, 9, 3, 500)
 
 find_clump_ecoli()
